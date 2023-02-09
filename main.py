06b45ef3dce2636b8ac9668c8a8cc13d5fa613b0
kayyyon <tyinaty@gmail.com>
2023-02-09 20:55:32 +1000

    Сменила язык

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+n = random.randrange(1,10)
+guess = int(input("Введите любое число: ")) #!
+while n!= guess:
+    if guess < n:
+        print("Загаданное число больше!")
+        guess = int(input("Введите число снова: "))
+    elif guess > n:
+        print("Загаданное число меньше!")
+        guess = int(input("Введите число снова: "))
+    else:
+      break
+print("Вы угадали!")
